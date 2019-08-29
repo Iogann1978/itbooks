80f6784ccf5e63e9b67dbd0d65eb05c9b36357ab
Iogann1978 <iogann1978@gmail.com>
2019-08-29 15:17:58 +0300

    Ввел enum Views

--- src/main/java/ru/home/itbooks/controller/AbstractController.java
+++ src/main/java/ru/home/itbooks/controller/AbstractController.java
@@ -8,19 +8,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractController<T0, T1 extends ItemService<T0>> {
+    enum Views {
+        View, List, Edit, Add, Del, Error;
+    }
+
     private T1 service;
-    private final Map<String, String> htmls = new HashMap<String, String>();
+    private final Map<Views, String> htmls = new HashMap<>();
 
     public AbstractController(T1 service) {
         this.service = service;
-        htmls.put("error", "error.html");
+        htmls.put(Views.Error, "error.html");
     }
 
-    protected void setViewHtml(String file) {htmls.put("view", file);}
-    protected void setListHtml(String file) {htmls.put("list", file);}
-    protected void setEditHtml(String file) {htmls.put("edit", file);}
-    protected void setAddHtml(String file) {htmls.put("add", file);}
-    protected void setDelHtml(String file) {htmls.put("del", file);}
+    protected void setViewHtml(String file) {
+        htmls.put(Views.View, file);
+    }
+    protected void setListHtml(String file) {
+        htmls.put(Views.List, file);
+    }
+    protected void setEditHtml(String file) {
+        htmls.put(Views.Edit, file);
+    }
+    protected void setAddHtml(String file) {
+        htmls.put(Views.Add, file);
+    }
+    protected void setDelHtml(String file) {
+        htmls.put(Views.Del, file);
+    }
     abstract protected void itemModel(Model model, T0 item);
     abstract protected void listModel(Model model, Iterable list);
 
@@ -28,10 +42,10 @@ public abstract class AbstractController<T0, T1 extends ItemService<T0>> {
         val item = service.findById(id);
         if(item.isPresent()) {
             itemModel(model, item.get());
-            return htmls.get("view");
+            return htmls.get(Views.View);
         } else {
             model.addAttribute("error", text_error);
-            return htmls.get("error");
+            return htmls.get(Views.Error);
         }
     }
 
@@ -42,23 +56,23 @@ public abstract class AbstractController<T0, T1 extends ItemService<T0>> {
             val list = service.findAll();
             listModel(model, list);
         }
-        return htmls.get("list");
+        return htmls.get(Views.List);
     }
 
     protected String edit(Model model, Long id) {
         val item = service.findById(id);
         if(item.isPresent()) {
             itemModel(model, item.get());
-            return htmls.get("edit");
+            return htmls.get(Views.Edit);
         } else {
             model.addAttribute("error", "Книга не найдена!");
-            return htmls.get("error");
+            return htmls.get(Views.Error);
         }
     }
 
     protected String add(Model model, T0 item) {
         itemModel(model, item);
-        return htmls.get("add");
+        return htmls.get(Views.Add);
     }
 
     protected String save(T0 item) {
@@ -70,10 +84,10 @@ public abstract class AbstractController<T0, T1 extends ItemService<T0>> {
         val item = service.findById(id);
         if(item.isPresent()) {
             itemModel(model, item.get());
-            return htmls.get("del");
+            return htmls.get(Views.Del);
         } else {
             model.addAttribute("error", "Книга не найдена!");
-            return htmls.get("error");
+            return htmls.get(Views.Error);
         }
     }
 
---
+++ src/main/java/ru/home/itbooks/controller/Views.java
@@ -0,0 +1,2 @@
+package ru.home.itbooks.controller;
+

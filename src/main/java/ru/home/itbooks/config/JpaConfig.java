d88311168847fcaf1872384a4b959c6c578cb686
Iogann1978 <iogann1978@gmail.com>
2021-02-17 23:55:02 +0300

    correct add_tag.html form

--- src/main/java/ru/home/itbooks/config/JpaConfig.java
+++ src/main/java/ru/home/itbooks/config/JpaConfig.java
@@ -3,6 +3,7 @@ package ru.home.itbooks.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @Configuration
 @EnableJpaRepositories(basePackages = {"ru.home.itbooks.repository"})
 public class JpaConfig {

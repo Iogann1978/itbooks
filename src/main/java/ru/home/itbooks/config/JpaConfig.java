b687ba851e0c687bf0dfc6b8cc0746cd3c9f9414
Iogann1978 <iogann1978@gmail.com>
2021-02-19 16:38:11 +0300

    add jaxb for java 11

--- src/main/java/ru/home/itbooks/config/JpaConfig.java
+++ src/main/java/ru/home/itbooks/config/JpaConfig.java
@@ -3,7 +3,6 @@ package ru.home.itbooks.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
 @Configuration
 @EnableJpaRepositories(basePackages = {"ru.home.itbooks.repository"})
 public class JpaConfig {
